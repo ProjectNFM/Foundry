cc57af78affaf4bc26af1816c23cdfc4ee044e75
estefanysuarez <laura.suarez@mail.mcgill.ca>
2026-04-08 16:06:53 -0400

    Added Neurosoft DataModule

--- foundry/data/datamodules/__init__.py
+++ foundry/data/datamodules/__init__.py
@@ -1,5 +1,13 @@
 from .base import NeuralDataModule
 from .physionet import PhysionetDataModule
 from .ajile import AjileDataModule
+from .neurosoft import NeurosoftMinipigs2026DataModule
+from .neurosoft import NeurosoftMonkeys2026DataModule
 
-__all__ = ["NeuralDataModule", "PhysionetDataModule", "AjileDataModule"]
+__all__ = [
+    "NeuralDataModule",
+    "PhysionetDataModule",
+    "AjileDataModule",
+    "NeurosoftMinipigs2026DataModule",
+    "NeurosoftMonkeys2026DataModule",
+]
---
+++ foundry/data/datamodules/neurosoft.py
@@ -0,0 +1,86 @@
+from torch_brain.dataset import Dataset
+from auditorydecoding import (
+    NeurosoftDataset,
+    NeurosoftMinipigs2026,
+    NeurosoftMonkeys2026,
+)
+from auditorydecoding.data.neurosoft_pipeline import (
+    ON_VS_OFF_TO_ID,
+    STIM_FREQUENCY_TO_ID,
+)
+from foundry.data.datamodules.base import NeuralDataModule
+from typing import Optional, Callable, Literal, Type
+
+
+class NeurosoftDataModule(NeuralDataModule):
+    TASK_TO_READOUT = {
+        "on_vs_off": ["neurosoft_on_vs_off"],
+        "acoustic_stim": ["neurosoft_acoustic_stim"],
+    }
+
+    READOUT_CLASS_NAMES: dict[str, list[str]] = {
+        "on_vs_off": list(ON_VS_OFF_TO_ID.keys()),
+        "acoustic_stim": list(STIM_FREQUENCY_TO_ID.keys()),
+    }
+
+    def __init__(
+        self,
+        dataset_class: Type[NeurosoftDataset],
+        root: str,
+        batch_size: int = 32,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+        window_length: Optional[float] = None,
+        transforms: Optional[list[Callable]] = None,
+        tokenizer: Optional[Callable] = None,
+        seed: int = 42,
+        split_type: Optional[
+            Literal[
+                "intersubject",
+                "intersession",
+                "intrasession",
+                "intrasession-block",
+                "intrasession-causal",
+            ]
+        ] = None,
+        task_type: Optional[
+            Literal["on_vs_off", "acoustic_stim"]
+        ] = "on_vs_off",
+        fold_number: Optional[int] = 0,
+        recording_ids: Optional[list[str]] = None,
+    ):
+        dataset_kwargs = {
+            "recording_ids": recording_ids,
+            "split_type": split_type,
+            "task_type": task_type,
+            "fold_num": fold_number,
+        }
+        super().__init__(
+            dataset_class=dataset_class,
+            root=root,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            window_length=window_length,
+            transforms=transforms,
+            tokenizer=tokenizer,
+            seed=seed,
+            dataset_kwargs=dataset_kwargs,
+            task_type=task_type,
+        )
+
+    def get_recording_ids(self) -> list[str]:
+        return sorted(self.dataset.recording_ids)
+
+    def get_channel_ids(self) -> list[str]:
+        return sorted(self.dataset.get_channel_ids())
+
+
+class NeurosoftMinipigs2026DataModule(NeurosoftDataModule):
+    def __init__(self, **kwargs):
+        super().__init__(dataset_class=NeurosoftMinipigs2026, **kwargs)
+
+
+class NeurosoftMonkeys2026DataModule(NeurosoftDataModule):
+    def __init__(self, **kwargs):
+        super().__init__(dataset_class=NeurosoftMonkeys2026, **kwargs)
