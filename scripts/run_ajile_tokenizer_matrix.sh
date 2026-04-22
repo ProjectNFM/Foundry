9d9432b7b3c7dfd8cb07d5366164f7d699e4cf25
Milo Sobral <milosobral@gmail.com>
2026-04-22 18:40:05 +0000

    Added some option to use a different effective batch size to make training faster

--- scripts/run_ajile_tokenizer_matrix.sh
+++ scripts/run_ajile_tokenizer_matrix.sh
@@ -8,9 +8,34 @@ TOKENIZERS=(
   spatial_session_cwt
 )
 
+EFFECTIVE_BATCH_SIZE=1024
+MAX_BATCH_SIZES=(
+  64
+  128
+  512
+  512
+)
+
 WANDB_GROUP="AJILE12_INTRASESSION_TOKENIZER_MATRIX"
 EXPERIMENT="tokenizer_explore/poyo_ajile_tokenizer_matrix"
 JOB_PIDS=()
+
+accum_steps() {
+  local effective="$1"
+  local batch="$2"
+
+  if ((batch <= 0)); then
+    echo "ERROR: batch size must be > 0, got ${batch}" >&2
+    exit 1
+  fi
+
+  if ((effective % batch != 0)); then
+    echo "ERROR: effective batch size ${effective} not divisible by ${batch}" >&2
+    exit 1
+  fi
+
+  echo $((effective / batch))
+}
 
 stop_all_jobs() {
   if ((${#JOB_PIDS[@]} == 0)); then
@@ -31,11 +56,15 @@ trap stop_all_jobs INT TERM
 run_behavior_job() {
   local gpu_id="$1"
   local tokenizer="$2"
+  local batch_size="$3"
+  local accumulate_grad_batches="$4"
 
   CUDA_VISIBLE_DEVICES="${gpu_id}" \
     uv run python main.py \
     experiment="${EXPERIMENT}" \
     model/tokenizer="${tokenizer}" \
+    hyperparameters.batch_size="${batch_size}" \
+    trainer.accumulate_grad_batches="${accumulate_grad_batches}" \
     data.task_type=behavior \
     model.zero_output_timestamps=true \
     trainer.callbacks.early_stopping.monitor=val/ajile_active_behavior_f1 \
@@ -54,11 +83,15 @@ run_behavior_job() {
 run_pose_job() {
   local gpu_id="$1"
   local tokenizer="$2"
+  local batch_size="$3"
+  local accumulate_grad_batches="$4"
 
   CUDA_VISIBLE_DEVICES="${gpu_id}" \
     uv run python main.py \
     experiment="${EXPERIMENT}" \
     model/tokenizer="${tokenizer}" \
+    hyperparameters.batch_size="${batch_size}" \
+    trainer.accumulate_grad_batches="${accumulate_grad_batches}" \
     module=regression \
     data.task_type=pose_estimation \
     model.zero_output_timestamps=false \
@@ -75,12 +108,21 @@ run_pose_job() {
   JOB_PIDS+=("$!")
 }
 
+if ((${#TOKENIZERS[@]} != ${#MAX_BATCH_SIZES[@]})); then
+  echo "ERROR: TOKENIZERS and MAX_BATCH_SIZES must have the same length" >&2
+  exit 1
+fi
+
 for idx in "${!TOKENIZERS[@]}"; do
-  run_behavior_job "${idx}" "${TOKENIZERS[$idx]}"
+  batch_size="${MAX_BATCH_SIZES[$idx]}"
+  accumulate_grad_batches="$(accum_steps "${EFFECTIVE_BATCH_SIZE}" "${batch_size}")"
+  run_behavior_job "${idx}" "${TOKENIZERS[$idx]}" "${batch_size}" "${accumulate_grad_batches}"
 done
 
 for idx in "${!TOKENIZERS[@]}"; do
-  run_pose_job "$((idx + 3))" "${TOKENIZERS[$idx]}"
+  batch_size="${MAX_BATCH_SIZES[$idx]}"
+  accumulate_grad_batches="$(accum_steps "${EFFECTIVE_BATCH_SIZE}" "${batch_size}")"
+  run_pose_job "$((idx + 4))" "${TOKENIZERS[$idx]}" "${batch_size}" "${accumulate_grad_batches}"
 done
 
 exit_code=0
